4cb0ac03cc5b8562444552dbbea8883f42edcb3b
GEORGHIY <georgyurov@yandex.ru>
2026-03-09 02:42:01 +0300

    урок 3

--- IntensiveJS-2/js/bg-element.js
+++ IntensiveJS-2/js/bg-element.js
@@ -1,9 +1,12 @@
-document.addEventListener('DOMContentLoaded', ()=> {
-  const elList = document.querySelectorAll('.set-bg')
+export const setbg = (elem) => {
+    const elList = elem.querySelectorAll('.set-bg')
 
   elList.forEach(element => {
     const src = element.dataset.setbg
     element.style.backgroundImage = `url(${src})`
   });
+ }
 
+document.addEventListener('DOMContentLoaded', ()=> {
+  setbg(document)
 })
